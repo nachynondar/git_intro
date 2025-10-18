e62fa4ae4e71ce2a4f9b53690dfb9175203c3743
nachyn ondar <ondar.nb@phystech.edu>
2025-10-18 12:44:43 -0700

    modified pom.xml

---
+++ src/test/com/example/demo/MyTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyTest {
+  @Test
+  void testSum() {
+    assertEquals(2, 1 + 1);
+  }
+}
