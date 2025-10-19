d3b7239bf19c81fa0611b780745ce5add1b6b81b
nachyn ondar <ondar.nb@phystech.edu>
2025-10-18 17:54:38 -0700

    modified settings

--- src/test/com/example/demo/MyTest.java
+++ src/test/com/example/demo/MyTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MyTest {
+public class MyTest {
   @Test
-  void testSum() {
+  public void testSum() {
     assertEquals(2, 1 + 1);
   }
 }
